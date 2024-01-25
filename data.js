730f9dfcbec388cf2f9acb2438b0321d1f06e84f
iismaflores <ismaelfloresstampone2@gmail.com>
2024-01-25 13:58:17 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const libros = [
+    {id: 1, titulo: 'Titulo 1', autor: 'Autor 1'},
+    {id: 2, titulo: 'Titulo 2', autor: 'Autor 2'},
+    {id: 3, titulo: 'Titulo 3', autor: 'Autor 3'}
+];
+
+module.exports = libros;
